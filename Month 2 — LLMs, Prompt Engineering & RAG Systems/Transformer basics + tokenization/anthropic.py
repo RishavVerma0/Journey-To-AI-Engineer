a73c5f3a2a3c61286968dd9fdd3732a6034833cb
RishavVerma0 <vermarishav2303@gmail.com>
2026-06-14 10:51:16 +0530

    added on 14th June

--- Month 2 — LLMs, Prompt Engineering & RAG Systems/Transformer basics + tokenization/anthropic.py
+++ Month 2 — LLMs, Prompt Engineering & RAG Systems/Transformer basics + tokenization/anthropic.py
@@ -4,9 +4,9 @@
 
 from openai import OpenAI
 
-api_key = "csk-*******************************************"
-base_url = "https://api.cerebras.ai/v1"
-model = "gpt-oss-120b"
+api_key = "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"
+base_url = "https://api.anthropic.com/v1/"
+model = "claude-sonnet-4-5"
 
 client = OpenAI(api_key=api_key, base_url=base_url)
 
