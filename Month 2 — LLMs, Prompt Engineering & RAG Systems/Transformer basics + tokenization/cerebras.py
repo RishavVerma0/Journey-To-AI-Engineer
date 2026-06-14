4410dff25dd5976cb9d086b64081a3569208bcdc
RishavVerma0 <vermarishav2303@gmail.com>
2026-06-14 10:13:22 +0530

    added on 14th June

---
+++ Month 2 — LLMs, Prompt Engineering & RAG Systems/Transformer basics + tokenization/cerebras.py
@@ -0,0 +1,27 @@
+# I am going to create a sample OpenAI API based system for testing purposes
+#This system will work with OpenAI, Gemini, Groq, Openrouter, Cerebras... etc
+
+
+from openai import OpenAI
+
+api_key = "csk-ch8ex5dkt5mtyxt4m22cevn8k8j2nw85nvcxm4fm2jpn4ph5"
+base_url = "https://api.cerebras.ai/v1"
+model = "gpt-oss-120b"
+
+client = OpenAI(api_key=api_key, base_url=base_url)
+
+prompt = input("\n User >>")
+
+while prompt.strip().lower() != "exit":
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "system", "content" : "You are a helpful Assistant."},
+            {"role": "user", "content" : prompt}
+        ]
+    )
+    print("Response: ", response.choices[0].message.content)
+
+
+    prompt = input("\n User >>") 
