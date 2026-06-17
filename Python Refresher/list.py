7babcf745fef33072b0ab4bec715ca893b089bab
RishavVerma0 <vermarishav2303@gmail.com>
2026-06-17 17:34:04 +0530

    Updated on 17th June

---
+++ Python Refresher/list.py
@@ -0,0 +1,33 @@
+## I am refreshing my list concepts in this code 
+
+rishav = ['angry','happy','sad','confused','crying','focused']
+
+#My list(unsorted)
+nums = [20,10,40,90,80,50,70,30]
+
+#sum of the list
+sum = sum(nums)
+print(sum)
+
+#max of the list
+print(max(nums))
+
+#min of the list
+print(min(nums))
+
+#sort in ascending order
+sort_nums = sorted(nums, reverse=False)
+print(sort_nums)
+
+#sort in descending order
+sort_nums = sorted(nums, reverse=True)
+print(sort_nums)
+
+#printing first 3 values
+print(nums[:3])
+
+#printing last 3 values
+print(nums[-3:])
+
+#printing every second element
+print(nums[::2])
