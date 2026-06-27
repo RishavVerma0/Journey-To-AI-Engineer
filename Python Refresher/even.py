5b81c1f09ed1ae3b93375636c8dc764948119ed2
RishavVerma0 <vermarishav2303@gmail.com>
2026-06-27 19:49:21 +0530

    Updated on 27th June

--- Python Refresher/even.py
+++ Python Refresher/even.py
@@ -1,13 +1,7 @@
-# I am writing a code to find the even numbers from a list
+# I am writing a code to find the even numbers from a list using list comprehension
 
 def even_num(nums):
-    evens = []
-
-    for num in nums:
-        if num % 2 == 0:
-            evens.append(num)
-
-    return evens
+    return [num for num in nums if num % 2 == 0]
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(even_num(numbers))
