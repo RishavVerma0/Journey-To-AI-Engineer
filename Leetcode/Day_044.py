fa68e2d134196c65a41d8e6408c987a238e2cfde
RishavVerma0 <vermarishav2303@gmail.com>
2026-06-28 19:36:24 +0530

    Updated on 28th June

---
+++ Leetcode/Day_044.py
@@ -0,0 +1,32 @@
+# 1351. Count Negative Numbers in a Sorted Matrix
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+ 
+
+# Example 1:
+
+# Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+# Output: 8
+# Explanation: There are 8 negatives number in the matrix.
+# Example 2:
+
+# Input: grid = [[3,2],[1,0]]
+# Output: 0
+
+from typing import List
+
+
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        count = 0
+
+        for row in grid:
+            for cell in row:
+                if cell < 0:
+                    count += 1
+        return count
