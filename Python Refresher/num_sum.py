ae0e0fd54663bbecbdb6ec3c4deeb23592033e1f
RishavVerma0 <vermarishav2303@gmail.com>
2026-06-29 22:31:19 +0530

    Updated on 28th June

---
+++ Python Refresher/num_sum.py
@@ -0,0 +1,7 @@
+def sum_numbers(*args):
+    return sum(args)
+
+# Example usage
+print(sum_numbers(1, 2, 3))          # 6
+print(sum_numbers(10, 20, 30, 40))   # 100
+print(sum_numbers())                 # 0
