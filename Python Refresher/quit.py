f3f69f596d9e71c07cc30de61653d0a809f1f1ad
RishavVerma0 <vermarishav2303@gmail.com>
2026-07-01 22:22:26 +0530

    Updated on 1st July

---
+++ Python Refresher/quit.py
@@ -0,0 +1,9 @@
+while True:
+    user_input = input("Enter something (type 'quit' to exit): ")
+
+    if user_input == "quit":
+        break
+
+    print("You entered:", user_input)
+
+print("Goodbye!")
