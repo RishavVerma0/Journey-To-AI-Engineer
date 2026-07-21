ede24d5b3edf2f835902c01ef68c3e5545f63f3e
RishavVerma0 <vermarishav2303@gmail.com>
2026-07-21 21:19:20 +0530

    Updated on 21st July

---
+++ Leetcode/Day_068.py
@@ -0,0 +1,43 @@
+# 347. Top K Frequent Elements
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,1,2,2,3], k = 2
+
+# Output: [1,2]
+
+# Example 2:
+
+# Input: nums = [1], k = 1
+
+# Output: [1]
+
+# Example 3:
+
+# Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+
+# Output: [1,2]
+
+from collections import Counter
+import heapq
+from typing import List
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = Counter(nums)
+        heap = []
+
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [num for count, num in heap]
